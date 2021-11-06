f570a7793a9c94867b8aaea8c8ab4e9347b4a5b8
Patryk Kuciński <pkucinski@Air-Patryk.lan>
2021-11-06 09:57:21 +0100

    add login site and fixed navbar

--- wypozyczalnia_gier/urls.py
+++ wypozyczalnia_gier/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from main.views import index
+from main.views import index, login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', login),
     path('', index)
 ]
