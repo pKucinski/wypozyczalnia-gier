9dd5988a7adaadf3fd7ee0d89a8f87f3fc9b1478
pKucinski <kucinskipatryk@icloud.com>
2021-11-13 09:29:48 +0100

    add db

--- main/models.py
+++ main/models.py
@@ -11,10 +11,10 @@ class Categories(models.Model):
 
 class Product(models.Model):
     id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=50)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    amount = models.DecimalField(max_digits=6, decimal_places=0)
+    title = models.CharField(max_length=50, blank=False)
+    description = models.TextField(blank=False)
+    price = models.DecimalField(max_digits=6, decimal_places=2, blank=False)
+    amount = models.DecimalField(max_digits=6, decimal_places=0, default=0)
     buy_counter = models.DecimalField(max_digits=6, decimal_places=0, default=0, editable=False)
     category = models.ForeignKey(Categories, on_delete=models.CASCADE)
     image = CloudinaryField('image')
