911f7738786f5386442d7cc04c1540cd76df815d
pKucinski <kucinskipatryk@icloud.com>
2021-11-24 22:30:45 +0100

    first commit

--- wypozyczalnia-gier/settings.py
+++ wypozyczalnia-gier/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['dice-play.herokuapp.com', '127.0.0.1', 'localhost', 'diceplay.xyz', 'www.diceplay.xyz']
+ALLOWED_HOSTS = ['dice-play.herokuapp.com', 'diceplay-test.herokuapp.com', '127.0.0.1', 'localhost', 'diceplay.xyz', 'www.diceplay.xyz']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
