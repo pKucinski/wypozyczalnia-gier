dd492c83b0c2674def147df3a924b41ca8dfb6e8
pKucinski <kucinskipatryk@icloud.com>
2021-11-24 22:00:27 +0100

    add register

--- wypozyczalnia-gier/settings.py
+++ wypozyczalnia-gier/settings.py
@@ -86,10 +86,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-if DEBUG:
-     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#Delete if local
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = ['static']
 
 MEDIA_URL = '/media/'
 
