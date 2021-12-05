1f8fe4c023be2595c1720e56a1091d3bdc370199
blazej2533 <bszczesny59@gmail.com>
2021-12-05 19:38:46 +0100

    addNewsleter

--- main/views.py
+++ main/views.py
@@ -2,9 +2,8 @@
 from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
+from django.core.mail import send_mail
 
-from django.db.models import Sum
-from . import form
 from .forms import SignUpForm
 from django.contrib.auth import login
 from django.contrib.auth import logout
@@ -19,11 +18,6 @@ from django.db.models import Avg
 def index(request):
     products = Product.objects.all().order_by("-id")
     return render(request, 'index.html', {'products': products})
-
-
-
-
-
 
 def login_page(request):
     return render(request, 'registration/login.html')
@@ -91,6 +85,14 @@ def show_search(request):
         return render(request, "search.html", {'searched':searched,'product':product,'product_count':product_count})
     else:
         return render(request, "search.html", {})
+
+def send_Email(request):
+    if request.method=="POST":
+        sendemailtome = request.POST['sendemailtome']
+    #l:diceplayonline@gmail.com
+    #h:Diceplay1!
+    send_mail('DicePlay - Newsleter','Czesc, Miło nam że zapisałeś się do naszego newsletera!, od teraz będziesz otrzymywał od nas wiadomości z promocjami.  Pozdrawiamy, zespół DicePlay','diceplayonline@gmail.com',[sendemailtome],fail_silently=False)
+    return render(request,'email.html')
 
 def show_product(request, id):
     show_product = get_object_or_404(Product, pk=id)
