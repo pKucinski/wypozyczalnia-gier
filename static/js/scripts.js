ff3092ec05294e3ebc04a7f0fd2c0e262ee184d6
blazej2533 <bszczesny59@gmail.com>
2021-12-05 13:58:15 +0100

    layout Basket

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -20,7 +20,22 @@ function updateSubTotal()
             var objCells = myTab.rows.item(i).cells;
             sum+=parseFloat(objCells[4].innerHTML);
         }
-        console.log(sum);
-  document.getElementById("sumOfProducts").innerHTML = sum.toFixed(2)+"zł";
 
+  document.getElementById("sumOfProducts").innerHTML = sum.toFixed(2)+"zł";
+  document.getElementById("sumOfProducts2").innerHTML = sum.toFixed(2)+"zł";
+
+
+}
+
+function countVat()
+{
+  var myTab = document.getElementById('basketTable');
+    var sum=0;
+        for (i = 1; i < myTab.rows.length-1; i++) {
+            var objCells = myTab.rows.item(i).cells;
+            sum+=parseFloat(objCells[4].innerHTML);
+        }
+    document.getElementById("toPay").innerHTML = sum.toFixed(2)+"zł";
+       sum=(sum-parseInt(sum)*0.77);
+    document.getElementById("vat").innerHTML = sum.toFixed(2)+"zł";
 }
